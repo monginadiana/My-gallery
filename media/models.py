c5ab10c75aabf0795c93b6d7c06992c7b027d612
monginadiana <diana.ondieki@student.moringa.com>
2021-11-26 22:56:35 +0300

    addition of the class image with the name, description and photo features

--- media/models.py
+++ media/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    photo = CloudinaryField('photo')
+    name =models.CharField(max_length=20)
+    description=models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
