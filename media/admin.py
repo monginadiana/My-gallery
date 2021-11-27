5e788d69a77400217ab72805248508b50f7d949c
monginadiana <diana.ondieki@student.moringa.com>
2021-11-28 00:47:52 +0300

    creation of the location model in the admins.py

--- media/admin.py
+++ media/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import  Category, Images
+from .models import  Category, Images, Location
 
 admin.site.register(Images)
 admin.site.register(Category)
-# admin.site.register(tags)
+admin.site.register(Location)
