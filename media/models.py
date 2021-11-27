f3b4e38937cbae6144dffb1b2628420127007c32
monginadiana <diana.ondieki@student.moringa.com>
2021-11-27 22:27:41 +0300

    creation of the category models

--- media/models.py
+++ media/models.py
@@ -22,3 +22,12 @@ class Images(models.Model):
     def search_by_name(cls,search_term):
         media = cls.objects.filter(name__icontains=search_term)
         return media
+
+# category model
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+     # save category to database
+    def save_category(self):
+        self.save()
+
