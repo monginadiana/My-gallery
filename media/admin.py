70a238e7e1f7cb53af3cc0ad057405373cfd63c8
monginadiana <diana.ondieki@student.moringa.com>
2021-11-27 22:28:46 +0300

    registering of the category model in the admin

--- media/admin.py
+++ media/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import  Images
+from .models import  Category, Images
 
 admin.site.register(Images)
-# admin.site.register(Article)
+admin.site.register(Category)
 # admin.site.register(tags)
