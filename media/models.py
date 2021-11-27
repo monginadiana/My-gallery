154be31e7949e263e072abd0af38c5f6d658ee2d
monginadiana <diana.ondieki@student.moringa.com>
2021-11-27 23:37:20 +0300

    addition of image category in the index.html

--- media/models.py
+++ media/models.py
@@ -1,14 +1,40 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+# category model
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+     #save category to database
+    def save_category(self):
+        self.save()
+    
+    # update category to database
+    def update_category(self, name):
+        self.name = name
+        self.save()
+        
+    # delete category to database
+    def delete_category(self):
+        self.delete()
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Images(models.Model):
     image = CloudinaryField('image')
     name =models.CharField(max_length=20)
     description=models.CharField(max_length=20)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
     
     
     def save_image(self):
+        self.save()
+    
+    def update_image(self, name, description, category):
+        self.name = name
+        self.description = description
+        self.category = category
         self.save()
     
     # delete image from database
@@ -23,20 +49,4 @@ class Images(models.Model):
         media = cls.objects.filter(name__icontains=search_term)
         return media
 
-# category model
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    
-     # save category to database
-    def save_category(self):
-        self.save()
-    
-    # update category to database
-    def update_category(self, name):
-        self.name = name
-        self.save()
-        
-    # delete category to database
-    def delete_category(self):
-        self.delete()
 
