e96a9a53be0140ede7fad2282dac86397224bc66
monginadiana <diana.ondieki@student.moringa.com>
2021-11-29 15:28:38 +0300

    correction of error during deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -75,7 +75,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap4',
-    'media',
+    'media.apps.MediaConfig',
     'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -166,7 +166,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
