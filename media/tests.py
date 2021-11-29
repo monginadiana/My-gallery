41de164946f34891f0e0605febcfef3633e6d120
monginadiana <diana.ondieki@student.moringa.com>
2021-11-29 08:29:57 +0300

    run test for the categories

--- media/tests.py
+++ media/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import  Category
 
 # Create your tests here.
+
+# category models test
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        """
+        Create a category for testing
+        """
+        Category.objects.create(name="Image Category")
+        
+    def test_category_name(self):
+        """
+        Test that the category name is correct
+        """
+        category = Category.objects.get(name="Image Category")
+        self.assertEqual(category.name, "Image Category")
+
+    def test_category_str(self):
+        """
+        Test that the category string representation is correct
+        """
+        category = Category.objects.get(name="Image Category")
+        self.assertEqual(str(category), "Image Category")
+    
+
+
+
